1998354e166b4638999e72b2c27f041067a22f8f
Jace <jaceduckworth4@gmail.com>
2025-04-01 14:12:06 -0400

    Added a small text chang

--- web_root/staff-landing.php
+++ web_root/staff-landing.php
@@ -38,7 +38,7 @@ for($i = 0; $i < sizeof($shiftData); $i++){
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="landing.php">Patient Portal System</a>
+            <a class="navbar-brand ps-3" href="landing.php">Staff Portal System</a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
