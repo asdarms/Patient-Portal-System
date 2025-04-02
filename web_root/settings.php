45007d5225d053332655f9e3477fdd914b08b098
Andrew d'Arms <andrewdarms@gmail.com>
2025-04-02 12:38:02 -0400

    Added User Settings Page (no functionality)

---
+++ web_root/settings.php
@@ -0,0 +1,9 @@
+<?php
+require 'generic.php';
+$user_id = getDatafromTable($conn, "user", ["username" => $_SESSION['username']])[0]['user_id'];
+$content_url = 'settings-content.php';
+?>
+<title>User Settings</title>
+<?php
+require 'landing-content.php';
+?>
