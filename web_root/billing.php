d3b31f86c759183a9b9a428c0e4c15d86ddfc6bb
Andrew d'Arms <andrewdarms@gmail.com>
2025-04-09 21:49:09 -0400

    moved user info to generic

--- web_root/billing.php
+++ web_root/billing.php
@@ -1,6 +1,5 @@
 <?php
 require 'generic.php';
-$user_id = getDatafromTable($conn, "user", ["username" => $_SESSION['username']])[0]['user_id'];
 //$staff_id = getDatafromTable($conn, "staff", ["staff_id"]);
 $patient = getDatafromTable($conn, "patient", ["user_id" => $user_id])[0];
 $bills = null;
--- web_root/generic.php
+++ web_root/generic.php
@@ -6,10 +6,14 @@ if (!isUserLoggedIn($conn)) {
     Header("Location: login.php");
 }
 $userData = getDatafromTable($conn, "user", ["username" => $_SESSION['username']])[0];
+$user_id = $userData['user_id'];
 $firstName = $userData['first_name'];
 if (sizeOf(getDatafromTable($conn, "staff", ["user_id" => $_SESSION["user_id"]])) > 0) {
-    $staff = True;
+    $adminMode = True;
+    $staff = getDatafromTable($conn, "staff", ["user_id" => $user_id])[0];
+
 } else {
-    $staff = False;
+    $adminMode = False;
+    $patient = getDatafromTable($conn, "patient", ["user_id" => $user_id])[0];
 }
 ?>
