1ab9e1f255ce857bb9f21942bad2698ee50eaf2f
Lexi <lexidunsmore03@gmail.com>
2025-04-10 00:46:05 -0400

    Fixed footer (slightly)

--- web_root/footer.php
+++ web_root/footer.php
@@ -1,6 +1,6 @@
 <?php
 echo '<div id="layoutAuthentication_footer">
-    <footer class="pb-4 pt-3 bg-light mt-auto">
+    <footer class="py-4 bg-light mt-auto">
         <div class="container-fluid px-4">
             <div class="d-flex align-items-center justify-content-between small">
                 <div class="text-muted">Copyright &copy; Patient Portal System 2025</div>
